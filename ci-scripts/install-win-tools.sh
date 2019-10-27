4adee7b9dad5e70fb1cf57e9441c8ad52c77e443
iketheadore <2589926+iketheadore@users.noreply.github.com>
2019-10-27 17:52:04 +1100

    Env

--- ci-scripts/install-win-tools.sh
+++ ci-scripts/install-win-tools.sh
@@ -1,4 +1,4 @@
 # Install nodejs with choco
 choco install nodejs --version=8.11.0 -y
-touch ~/.bashrc
-echo "export PATH='$PATH:/c/Program Files/nodejs/'" >> ~/.bashrc
+# touch ~/.bashrc
+# echo "export PATH='$PATH:/c/Program Files/nodejs/'" >> ~/.bashrc
