9b0a0cb57598c77d5556336ada2be11539b2f397
iketheadore <2589926+iketheadore@users.noreply.github.com>
2019-10-27 17:29:53 +1100

    Add bashrc

--- ci-scripts/install-win-tools.sh
+++ ci-scripts/install-win-tools.sh
@@ -1,3 +1,4 @@
 # Install nodejs with choco
 choco install nodejs --version=8.11.0 -y
-export PATH='$PATH:/c/Program Files/nodejs/'
+touch ~/.bashrc
+echo "export PATH='$PATH:/c/Program Files/nodejs/'" >> ~/.bashrc
