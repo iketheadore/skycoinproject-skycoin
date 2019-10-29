81e289f4736ce287f7509d6c2f9d3554176e1070
iketheadore <2589926+iketheadore@users.noreply.github.com>
2019-10-29 12:42:18 +1100

    Debug6

--- ci-scripts/install-tools.sh
+++ ci-scripts/install-tools.sh
@@ -8,8 +8,7 @@ if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
     choco install nodejs --version=8.11.0 -y
     echo 'export PATH="/c/Program Files/nodejs:${PATH}";' >> ~/.bashrc
 else
-    ls ~/.nvm
-    source $HOME/.nvm/nvm.sh
+    . $HOME/.nvm/nvm.sh  # This loads NVM
     nvm install 8.11.0
     nvm use 8.11.0
     make install-linters
