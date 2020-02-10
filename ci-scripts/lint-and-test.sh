435892ccee3a0f47dd23f632d581af84af9a280d
iketheadore <2589926+iketheadore@users.noreply.github.com>
2020-02-10 16:26:58 +1100

    Debug3

--- ci-scripts/lint-and-test.sh
+++ ci-scripts/lint-and-test.sh
@@ -7,7 +7,7 @@ set -e -o pipefail
 # make test-386
 # make test-amd64
 # make integration-tests-stable
-./ci-scripts/integration-test-stable.sh -r TestStableGetTransactionV2/page=1_limit=1 -v
+./ci-scripts/integration-test-stable.sh -r TestStableGetTransactionV2
 # make lint-ui
 # make build-ui-travis
 # make test-ui
