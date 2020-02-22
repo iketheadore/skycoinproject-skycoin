bd88e4e06a00890d799cddecf504340836c50858
iketheadore <2589926+iketheadore@users.noreply.github.com>
2020-02-22 17:40:23 +1100

    Parallel test 3

--- ci-scripts/lint-and-test.sh
+++ ci-scripts/lint-and-test.sh
@@ -2,19 +2,19 @@
 
 set -e -o pipefail
 
-# make check-newcoin
-# make lint
+make check-newcoin
+make lint
 
 if [[ ${TEST_SUIT} == "units" ]]; then
     echo "DO unit tests"
-    # make test-386
-    # make test-amd64
+    make test-386
+    make test-amd64
 elif [[ ${TEST_SUIT} == "integrations" ]]; then
     echo "DO integration tests"
-    # make integration-tests-stable
+    make integration-tests-stable
 fi
 
-# make lint-ui
-# make build-ui-travis
-# make test-ui
-# make test-ui-e2e
+make lint-ui
+make build-ui-travis
+make test-ui
+make test-ui-e2e
