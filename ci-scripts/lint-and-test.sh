269677a6f2ba5af3fb1f13b35ba724555af30b3d
iketheadore <2589926+iketheadore@users.noreply.github.com>
2020-02-23 17:59:00 +1100

    p33

--- ci-scripts/lint-and-test.sh
+++ ci-scripts/lint-and-test.sh
@@ -3,18 +3,18 @@
 set -e -o pipefail
 
 make check-newcoin
-make lint
+make build-ui-travis
 
 if [[ ${TEST_SUIT} == "units" ]]; then
     echo "DO unit tests"
+    make install-linters
+    make lint
+    make lint-ui
     make test-386
     make test-amd64
+    make test-ui
 elif [[ ${TEST_SUIT} == "integrations" ]]; then
     echo "DO integration tests"
     make integration-tests-stable
+    make test-ui-e2e
 fi
-
-make lint-ui
-make build-ui-travis
-make test-ui
-make test-ui-e2e
